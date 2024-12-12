fd70293e7487436b35f493a9f55f40b9a0633aa4
Zainab Bashir <ZainabBashir425>
2024-12-12 14:32:52 +0500

    Tailwind installed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
-      hello
-    </div>
+    <h1 className="text-3xl font-bold text-slate-700 underline">
+      Hello world!
+    </h1>
   )
 }
 
