7dc4bffdb5f46f7f1fa874161cc416d88e917284
JavariaTabassum <128834541+JavariaTabassum@users.noreply.github.com>
2025-01-01 12:01:30 +0500

    Commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <h1 className="text-3xl font-bold text-slate-700 underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default Home
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,30 +1,11 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Includes all JavaScript, TypeScript, and JSX/TSX files
-    "./public/index.html",        // Includes HTML files
-  ],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
-<<<<<<< HEAD
     extend: {},
     fontFamily: {
       'ibm-plex': ['"IBM Plex Sans"', 'sans-serif'],
       'inter': ['"Inter"', 'sans-serif'],
       'poppins': ['"Poppins"', 'sans-serif'], 
-=======
-    extend: {
-      colors: {
-        primary: '#EC008C',
-      },
-      spacing: {
-        '18rem': '18rem',
-      },
-      fontFamily: {
-        'Roboto': ['"Roboto"', 'serif'],
-        'Poppins':['"Poppins"','serif'],
-        'Abhaya': ['"Abhaya Libre"', 'serif'],
-        'Albert':['"Albert Sans"', 'serif'],
-      },
->>>>>>> 7bb2206dd608c449ccb18a39c7db3d8b1affd7d4
     },
   },
   plugins: [],
